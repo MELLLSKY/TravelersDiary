d17eecd41f291879bf25046e7a7bbdaa2f15961b
U180709034 <meliseky@gmail.com>
2022-12-12 21:28:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/travelersdiary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.travelersdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+
+
+
+}
