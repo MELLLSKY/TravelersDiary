5b63bbbbd4585a0affa05f16ca82615218d1c4a2
Melise Kaya <meliseky@gmail.com>
2022-12-22 19:40:27 +0300

    Firebase connection
    these changes are made by Melise Kaya

--- app/src/main/java/com/example/travelersdiary/MainActivity.java
+++ app/src/main/java/com/example/travelersdiary/MainActivity.java
@@ -6,12 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button newEventButton;
     Button editEventButton;
+    ImageView profilePhoto;
+    EditText goalsText;
+
+
 
 
 
@@ -20,13 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        profilePhoto=findViewById(R.id.img_profile);
+        goalsText=findViewById(R.id.txt_goalsview);
+
         newEventButton=findViewById(R.id.btn_new_event);
         newEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,CreatEventPage.class));
-
-
             }
         });
 
@@ -35,14 +45,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,CreatEventPage.class));
-
-
             }
         });
-
-
-
     }
+
 
 
 
